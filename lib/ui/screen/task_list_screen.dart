245b031c0509bfe784b24f3576d5939c32fee22d
Aidyl <jgarciaalbalah@gmail.com>
2022-05-23 00:58:13 -0400

    TaskListScreen: +BlocProvider

--- lib/ui/screen/task_list_screen.dart
+++ lib/ui/screen/task_list_screen.dart
@@ -1,22 +1,28 @@
-import 'package:basic_task_list/ui/ui_exporter.dart';
+import 'package:basic_task_list/app_exporter.dart';
 import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class TaskListScreen extends StatelessWidget {
   const TaskListScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Basic Task List"),
-      ),
-      body: ExpandableTheme(
-        data: const ExpandableThemeData(
-          iconColor: Colors.blue,
-          useInkWell: true,
+    return BlocProvider(
+      create: (context) => ItemBloc(
+        itemRepository: ItemServiceImplementation(),
+      )..add(const ItemFetchEvent()),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Basic Task List"),
         ),
-        child: const BodyWidget(),
+        body: ExpandableTheme(
+          data: const ExpandableThemeData(
+            iconColor: Colors.blue,
+            useInkWell: true,
+          ),
+          child: const BodyWidget(),
+        ),
       ),
     );
   }
