43a6c5b213038fb4f1122f9366b200479f08579d
Aidyl <jgarciaalbalah@gmail.com>
2022-05-23 00:27:04 -0400

    +ItemServiceImplementation

---
+++ lib/repository/service/item_service_impl.dart
@@ -0,0 +1,32 @@
+import 'package:basic_task_list/repository/repository_exporter.dart';
+import 'package:dio/dio.dart';
+
+class ItemServiceImplementation extends ItemService {
+  static final Dio _dio = Dio();
+
+  @override
+  Future<List<ItemModel>> getAllItems() async {
+    try {
+      List<Map<String, dynamic>> allItems = [];
+
+      // Get object info.
+      var response = await _dio.get(ApiConstants.baseURL);
+
+      allItems
+          .addAll(List<Map<String, dynamic>>.from(response.data["results"]));
+
+      return List<ItemModel>.from(allItems.map((x) => ItemModel.fromJson(x)));
+    } on DioError catch (exc) {
+      if (exc.response != null) {
+        if (exc.response!.statusCode == 404) {
+          throw Exception("You have reached the end of the tasks.");
+        } else {
+          throw Exception(
+              "${exc.response!.statusCode}: ${exc.response!.statusMessage}");
+        }
+      } else {
+        throw Exception("Couldn't fetch tasks. Is the device online?");
+      }
+    }
+  }
+}
