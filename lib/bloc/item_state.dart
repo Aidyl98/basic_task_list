b3deab2a5a3bcffaf3c12ec1bc9d60a01dbd11d0
Aidyl <jgarciaalbalah@gmail.com>
2022-05-23 00:43:05 -0400

    ItemState: + ItemSuccessState & ItemErrorState

--- lib/bloc/item_state.dart
+++ lib/bloc/item_state.dart
@@ -4,3 +4,27 @@ part of 'item_bloc.dart';
 abstract class ItemState {}
 
 class ItemInitial extends ItemState {}
+
+class ItemLoadingState extends ItemState {
+  final String message;
+
+  ItemLoadingState({
+    required this.message,
+  });
+}
+
+class ItemSuccessState extends ItemState {
+  final List<ItemModel> item;
+
+  ItemSuccessState({
+    required this.item,
+  });
+}
+
+class ItemErrorState extends ItemState {
+  final String error;
+
+  ItemErrorState({
+    required this.error,
+  });
+}
