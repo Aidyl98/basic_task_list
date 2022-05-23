f5d2494e2548f25a6ff65a8b817a38f2794d3cf9
Aidyl <jgarciaalbalah@gmail.com>
2022-05-22 23:44:13 -0400

    main: + _buildTheme

--- lib/main.dart
+++ lib/main.dart
@@ -13,46 +13,51 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Basic Task List',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        buttonTheme: ButtonThemeData(
-          buttonColor: Colors.blueGrey.shade100,
-          textTheme: ButtonTextTheme.primary,
-        ),
-        iconTheme: const IconThemeData(
-          color: Colors.blueGrey,
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.resolveWith<Color?>((s) {
-              if (s.contains(MaterialState.disabled)) {
-                return Colors.blueGrey.shade50;
-              } // Disabled button color
-              return Colors.blueGrey.shade100; // Enabled button color
-            }),
-            foregroundColor: MaterialStateProperty.resolveWith<Color?>((s) {
-              if (s.contains(MaterialState.disabled)) {
-                return Colors.grey;
-              } // Disabled button color
-              return Colors.black; // Enabled button color
-            }),
-          ),
-        ),
-        outlinedButtonTheme: OutlinedButtonThemeData(
-          style: ButtonStyle(
-            foregroundColor: MaterialStateProperty.resolveWith<Color?>((s) {
-              if (s.contains(MaterialState.disabled)) {
-                return Colors.blueGrey.shade400;
-              } // Disabled button color
-              return Colors.blueGrey; // Enabled button color
-            }),
-          ),
-        ),
-      ),
+      theme: _buildTheme(),
       initialRoute: '/',
       routes: {
         '/': (context) => const TaskListScreen(),
       },
+    );
+  }
+
+  ThemeData _buildTheme() {
+    return ThemeData(
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+      primarySwatch: Colors.blue,
+      buttonTheme: ButtonThemeData(
+        buttonColor: Colors.blueGrey.shade100,
+        textTheme: ButtonTextTheme.primary,
+      ),
+      iconTheme: const IconThemeData(
+        color: Colors.blueGrey,
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.resolveWith<Color?>((s) {
+            if (s.contains(MaterialState.disabled)) {
+              return Colors.blueGrey.shade50;
+            } // Disabled button color
+            return Colors.blueGrey.shade100; // Enabled button color
+          }),
+          foregroundColor: MaterialStateProperty.resolveWith<Color?>((s) {
+            if (s.contains(MaterialState.disabled)) {
+              return Colors.grey;
+            } // Disabled button color
+            return Colors.black; // Enabled button color
+          }),
+        ),
+      ),
+      outlinedButtonTheme: OutlinedButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.resolveWith<Color?>((s) {
+            if (s.contains(MaterialState.disabled)) {
+              return Colors.blueGrey.shade400;
+            } // Disabled button color
+            return Colors.blueGrey; // Enabled button color
+          }),
+        ),
+      ),
     );
   }
 }
