3cd9b5a63a5a12f4b0a6e8bac7c46b8f1344fabc
Aidyl <jgarciaalbalah@gmail.com>
2022-05-23 18:50:21 -0400

    ApiConstants: + editItem

--- lib/repository/api_constants.dart
+++ lib/repository/api_constants.dart
@@ -2,4 +2,5 @@ class ApiConstants {
   static const baseURL = "http://10.0.2.2:7777";
   static const findAll = "$baseURL/task/all";
   static const addItem = "$baseURL/task/create";
+  static const editItem = "$baseURL/task/edit";
 }
