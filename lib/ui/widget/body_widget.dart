fbab7914ed9a679e5165ca0eabe198485db0fa79
Aidyl <jgarciaalbalah@gmail.com>
2022-05-23 00:58:58 -0400

    BodyWidget: + BlocConsumer

--- lib/ui/widget/body_widget.dart
+++ lib/ui/widget/body_widget.dart
@@ -1,6 +1,7 @@
-import 'package:basic_task_list/ui/widget/widget_exporter.dart';
+import 'package:basic_task_list/app_exporter.dart';
 import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 // ignore: must_be_immutable
 class BodyWidget extends StatelessWidget {
@@ -8,52 +9,137 @@ class BodyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<ItemModel> _items = [];
+    final Size size = MediaQuery.of(context).size;
     final expandableController = ExpandableController(
       initialExpanded: false,
     );
 
-    return SingleChildScrollView(
-      child: ExpandableNotifier(
-        controller: expandableController,
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            ScrollOnExpand(
-              scrollOnExpand: true,
-              scrollOnCollapse: false,
-              child: ExpandablePanel(
-                theme: const ExpandableThemeData(
-                  headerAlignment: ExpandablePanelHeaderAlignment.center,
-                  tapBodyToExpand: true,
+    return Center(
+      child: BlocConsumer<ItemBloc, ItemState>(
+        listener: (context, itemState) {
+          // Loading Data.
+          if (itemState is ItemLoadingState) {
+            ScaffoldMessenger.of(context).clearSnackBars();
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: WidgetUtils.buildInfoText(
+                  text: itemState.message,
+                  context: context,
+                  size: size,
+                  color: Colors.black,
+                  textAlign: TextAlign.center,
                 ),
-                collapsed: const TextFieldCollapsedWidget(),
-                expanded: TextFieldExpandeddWidget(
-                  expandableController: expandableController,
+              ),
+            );
+            // Get Data, end of the list.
+          } else if (itemState is ItemSuccessState && itemState.item.isEmpty) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: WidgetUtils.buildInfoText(
+                  text: 'No more characters',
+                  context: context,
+                  size: size,
+                  color: Colors.black,
+                  textAlign: TextAlign.center,
                 ),
-                builder: (_, collapsed, expanded) {
-                  return Expandable(
-                    collapsed: collapsed,
-                    expanded: expanded,
-                    theme: const ExpandableThemeData(crossFadePoint: 0),
-                  );
-                },
+              ),
+            );
+            // Get Data Error.
+          } else if (itemState is ItemErrorState) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: WidgetUtils.buildInfoText(
+                  text: itemState.error,
+                  context: context,
+                  size: size,
+                  color: Colors.black,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            );
+          }
+          return;
+        },
+        builder: (context, itemState) {
+          // Loading Data
+          if (itemState is ItemInitial ||
+              itemState is ItemLoadingState && _items.isEmpty) {
+            return WidgetUtils.buildCircularProgressIndicator(context);
+            // Add the fetched data to the list.
+          } else if (itemState is ItemSuccessState) {
+            _items.addAll(itemState.item);
+            ScaffoldMessenger.of(context).clearSnackBars();
+            // Error View.
+          } else if (itemState is ItemErrorState && _items.isEmpty) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Reload recuest.
+                IconButton(
+                  iconSize: size.width / 5,
+                  color: Theme.of(context).colorScheme.secondary,
+                  splashColor: Theme.of(context).colorScheme.background,
+                  tooltip: "Try to fetch the data.",
+                  onPressed: () {
+                    BlocProvider.of<ItemBloc>(context)
+                        .add(const ItemFetchEvent());
+                  },
+                  icon: const Icon(
+                    Icons.refresh,
+                  ),
+                ),
+                // Show text error.
+                const SizedBox(height: 15),
+                WidgetUtils.buildInfoText(
+                  text: itemState.error,
+                  context: context,
+                  size: size,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            );
+          }
+          return SingleChildScrollView(
+            child: ExpandableNotifier(
+              controller: expandableController,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  ScrollOnExpand(
+                    scrollOnExpand: true,
+                    scrollOnCollapse: false,
+                    child: ExpandablePanel(
+                      theme: const ExpandableThemeData(
+                        headerAlignment: ExpandablePanelHeaderAlignment.center,
+                        tapBodyToExpand: true,
+                      ),
+                      collapsed: const TextFieldCollapsedWidget(),
+                      expanded: TextFieldExpandeddWidget(
+                        expandableController: expandableController,
+                      ),
+                      builder: (_, collapsed, expanded) {
+                        return Expandable(
+                          collapsed: collapsed,
+                          expanded: expanded,
+                          theme: const ExpandableThemeData(crossFadePoint: 0),
+                        );
+                      },
+                    ),
+                  ),
+                  Column(
+                    children: List.generate(
+                      _items.length,
+                      (index) => ListTileWidget(
+                          textList: _items[index].text.split(" ")),
+                    ),
+                  )
+                ],
               ),
             ),
-            _buildList(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  _buildList() {
-    return Column(
-      children: List.generate(
-        5,
-        (index) => ListTileWidget(
-            textList:
-                "Tomar cafe con @Jessica #Developer cuyo correo es jgarcia@gmail.com visitar para mas información www.google.com o tambien www.google.com"
-                    .split(" ")),
+          );
+        },
       ),
     );
   }
